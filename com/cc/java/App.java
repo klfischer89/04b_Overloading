f74cdd29b85e4957207d87c018966ff056ee7752
klfischer89 <fischer-k4@web.de>
2025-11-25 12:27:39 +0100

    test for learning overloading methods

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,14 +8,14 @@ public class App {
 		TestOverloading testOverloading = new TestOverloading();		
 		
 		// Überladen Methode sayHello2	
-		//  testOverloading.sayHello2("Hi", 1);
-		//  testOverloading.sayHello2(2,"Hu");
-		//  testOverloading.sayHello2();
+		testOverloading.sayHello2("Hi", 1);
+		testOverloading.sayHello2(2,"Hu");
+		testOverloading.sayHello2();
 		
 		
 		// Überladen Methode sayHello3
-		// testOverloading.sayHello3("Hallo", 1080);
-		// testOverloading.sayHello3("Hallo", "1080");
+		testOverloading.sayHello3("Hallo", 1080);
+		testOverloading.sayHello3("Hallo", "1080");
 		
 		
 	}
